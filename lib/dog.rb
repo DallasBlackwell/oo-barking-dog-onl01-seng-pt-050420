f418d7ad28b6743f495d1f3e6145397ecf4fdece
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-18 20:48:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,13 +27,13 @@ fido.bark
 puts fido.name 
 puts fido.bark
 
-class Dog
-  def bark
-    puts "Woof!"
-  end
-  def sit
-    puts "The Dog is sitting"
-  end
-end
+# class Dog
+#   def bark
+#     puts "Woof!"
+#   end
+#   def sit
+#     puts "The Dog is sitting"
+#   end
+# end
 
-fido = Dog.new
+# fido = Dog.new
