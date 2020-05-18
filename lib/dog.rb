224ab9d2d26cb7834002d95254769d749b31794b
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-18 20:31:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+class Dog
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+
+  def name
+    @this_dogs_name
+  end
+  
+  def bark=(dog_bark)
+    @this_dogs_bark = dog_bark
+  end  
+  
+  def bark
+    @this_dogs_bark
+      
+  end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+fido.bark = "woof!"
+
+puts fido.name 
+puts fido.bark
